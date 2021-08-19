23844246a6b44c01351e40566d2b8c150361adf2
Darren Foley <darren.foley@ucdconnect.ie>
2021-08-19 13:56:06 +0000

    First working commit

---
+++ create_warehouse.py
@@ -0,0 +1,2 @@
+# script that will spin up a new Redshift cluster if one does not exist
+import boto3
---
+++ update_git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config --global user.name "Darren Foley"
+git config --global user.email "darren.foley@ucdconnect.ie"
