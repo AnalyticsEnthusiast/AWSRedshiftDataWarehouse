0ab7d2f351f017de6defb929fb87cfdf7e6effe1
Darren Foley <darren.foley@ucdconnect.ie>
2021-08-25 15:04:27 +0000

    Updated update_git.sh script

--- update_git.sh
+++ update_git.sh
@@ -2,3 +2,13 @@
 
 git config --global user.name "Darren Foley"
 git config --global user.email "darren.foley@ucdconnect.ie"
+
+SSH_DIR=/root/.ssh
+
+[ ! -d "${SSH_DIR}" ] && mkdir -p "${SSH_DIR}" && echo "SSH  Directory Created"
+
+sudo apt-get install -y openssh-client
+
+cd $SSH_DIR
+
+ssh-keygen -f $SSH_DIR/id_rsa -t rsa -b 4096
